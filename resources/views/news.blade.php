f0cc229e3a58dade79fd6fce96cfe13d76693050
fgprayogo <fitrohgalih@gmail.com>
2018-12-13 00:57:05 +0700

    news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -20,31 +20,32 @@
 @endsection
 
 @section('content')
-   <div class="demo-1" background="BG.png" style="margin-top:60px;">
+   <div class="demo-1" background="BG.png" style="margin-top:60px">
       <div class="container">
          <div class="content">
             <!-- trianglify pattern container -->
             <div class="pattern pattern--hidden"></div>
             <!-- cards -->
-            <div class="wrapper">
+            <div class="wrapper" >
                <div class="card">
                   <div class="card__container card__container--closed">
-                     <svg class="card__image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 500" preserveAspectRatio="xMidYMid slice">
+                     <svg class="card__image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 500" preserveAspectRatio="xMidYMid slice" >
                         <defs>
-                           <clipPath id="clipPath1">
+                           <clipPath id="clipPath1" >
                               <!-- r = 992 = hyp = Math.sqrt(960*960+250*250) -->
                               <circle class="clip" cx="960" cy="250" r="992"></circle>
                            </clipPath>
                         </defs>
-                        <image clip-path="url(#clipPath1)" width="1920" height="500" xlink:href="img/a.jpg"></image>
+
+                        <image clip-path="url(#clipPath1)" width="1920" height="500" xlink:href="img/a.jpg" ></image>
                      </svg>
-                     <div class="card__content">
+                     <div class="card__content" >
                         <i class="card__btn-close fa fa-times"></i>
                         <div class="card__caption">
 
                            <p class="card__subtitle">Warga Meminta Jalur Pendakian Gunung Rinjani Lewat Senaru Dibuka</p>
                         </div>
-                        <div class="card__copy">
+                        <div class="card__copy" >
                            <div class="meta">
                               <img class="meta__avatar" src="img/authors/1.png" alt="author01" />
                               <span class="meta__author">Tulus Wijanarko</span>
